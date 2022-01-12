7bf4ba65c4202e978209870dfb99cd1b7c0835ab
szkkt <s20c1062rk@s.chibakoudai.jp>
2022-01-12 06:48:51 +0000

    Update

--- scripts/twice.py
+++ scripts/twice.py
@@ -7,15 +7,10 @@ from std_msgs.msg import Int32
 m = 0
 def cb(message):
     global m
-    m = math.sqrt(message.data)
-    #rospy.loginfo(message.data)
     for i in range(2,message.data):
         if message.data % i == 0:
             rospy.loginfo(message.data)
             return
-        #if message.data % i ==0:
-         #   rospy.loginfo('so')
-          #  return
 
 rospy.init_node('twice')
 sub = rospy.Subscriber('count_up', Int32, cb)
